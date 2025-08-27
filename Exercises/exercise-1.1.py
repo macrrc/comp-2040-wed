aa73e90f53a6fbebb612ed690a70e9b6d5a48eff
Chris Mac <cmac@rrc.ca>
2025-08-27 14:43:02 -0500

    add exercise 1 initial code

--- Exercises/exercise-1.1.py
+++ Exercises/exercise-1.1.py
@@ -1,1 +1,20 @@
-print("hello this is exercise 1.")
+# Calculating area of rectangles
+length1 = 10
+width1 = 5
+area_rectangle1 = length1 * width1
+
+length2 = 15
+width2 = 7
+area_rectangle2 = length2 * width2
+
+# Calculating area of circles
+radius1 = 4
+area_circle1 = 3.14 * (radius1 ** 2)
+
+radius2 = 6
+area_circle2 = 3.14 * (radius2 ** 2)
+
+print("Area of first rectangle:", area_rectangle1)
+print("Area of second rectangle:", area_rectangle2)
+print("Area of first circle:", area_circle1)
+print("Area of second circle:", area_circle2)
