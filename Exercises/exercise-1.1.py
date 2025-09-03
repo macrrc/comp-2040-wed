ef3f521cda7fc844c5dd7e5ad2dc756a55597f41
Chris Mac <cmac@rrc.ca>
2025-09-03 14:25:44 -0500

    refactor circult area calculation

--- Exercises/exercise-1.1.py
+++ Exercises/exercise-1.1.py
@@ -5,7 +5,7 @@ def calculate_rectangle_area(length, width):
     return length * width
 
 def calculate_circle_area(radius):
-    return 
+    return pi * (radius ** 2) 
 
 
 # Calculating area of rectangles
@@ -19,12 +19,12 @@ area_rectangle2 = calculate_rectangle_area(length2, width2)
 
 # Calculating area of circles
 radius1 = 4
-area_circle1 = pi * (radius1 ** 2)
+area_circle1 = calculate_circle_area(radius1)
 
 radius2 = 6
-area_circle2 = pi * (radius2 ** 2)
+area_circle2 = calculate_circle_area(radius2)
 
 print("Area of first rectangle:", area_rectangle1)
 print("Area of second rectangle:", area_rectangle2)
-print("Area of first circle:", area_circle1)
-print("Area of second circle:", area_circle2)
+print("Area of first circle:", round(area_circle1, 2))
+print("Area of second circle:", round(area_circle2, 2))
